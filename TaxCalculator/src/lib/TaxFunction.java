a499c83526cf5f38a72f6f807eca04064c0e09d5
gus-in <79630227+gus-in@users.noreply.github.com>
2022-04-20 23:29:10 +0800

    Menghapus comments
    Menghapus comments dan mendefinisikan parameter final.

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -14,23 +14,28 @@ public class TaxFunction {
 	 * 
 	 */
 	
+	private static final int incomeTaxRate = 0.05;
+	private static final int deductiblesUnmarriedAndChildless = 54000000;
+	private static final int maxNumberOfChildrenBonus = 3;
 	
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
 		
 		int tax = 0;
+		int bonusDeductiblesMarried = 4500000;
+		int bonusDeductiblesHaveChild = 1500000;
 		
 		if (numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
 		}
 		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
+		if (numberOfChildren > maxNumberOfChildrenBonus) {
+			numberOfChildren = maxNumberOfChildrenBonus;
 		}
 		
 		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
+			tax = (int) Math.round(incomeTaxRate * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (deductiblesUnmarriedAndChildless + bonusDeductiblesMarried + (numberOfChildren * bonusDeductiblesHaveChild))));
 		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+			tax = (int) Math.round(incomeTaxRate * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - deductiblesUnmarriedAndChildless));
 		}
 		
 		if (tax < 0) {
